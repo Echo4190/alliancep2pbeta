f35d8903119f57ba3ac60f3d94d20ee08ede66d1
larsontrent@gmail.com <larsontrent@gmail.com@cfbd0fa4-10d7-11df-9cc9-4389dd015ce2>
2011-02-24 06:41:16 +0000

    simplify test conditions; add console extension (though it doesn't work); add commentary

--- branches/trent-tests/src/org/alliance/misc/TestPlugIn.java
+++ branches/trent-tests/src/org/alliance/misc/TestPlugIn.java
@@ -12,46 +12,72 @@ import org.alliance.core.node.Friend;
 import org.alliance.core.node.Node;
 import org.alliance.core.plugins.ConsolePlugInExtension;
 import org.alliance.core.plugins.PlugIn;
+import org.alliance.launchers.console.Console.Printer;
 
 /**
+ * PlugIn to test the PlugIn and UICallback systems
  *
- * PlugIn to test the PlugIn and UICallback systems.
+ * We're trying to enforce the following design:
+ * <ul>
+ * <li>Make the PlugIn system initialize and shutdown all PlugIns and UICallbacks.</li>
+ * <li>Multiple loads and unloads of the core system or the UI (independently) will not affect the state of UICallbacks.</li>
+ * </ul>
+ *
+ * In order to test this, run in these modes:
+ * <ul>
+ * <li>standard: org.alliance.launchers.ui.Main</li>
+ * <li>testsuite: run 'ant-plugin-jar', then GenerateTestSuite, then org.alliance.launchers.testsuite.Main</li>
+ * <li>console: org.alliance.launchers.console.Main</li>
+ * </ul>
+ *
+ * ... and try various combinations of the following:
+ * <ul>
+ * <li>all: remove the alliance-test-plugin.jar and then add it, at least twice; result is exception-free</li>
+ * <li>testsuite only: Open UI & Stop, and for multiple people; result is exception-free</li>
+ * <li>testsuite only: Start & Stop, and for multiple people; result is exception-free</li>
+ * <li>console only: run 'ui' and 'killui'; result is exception-free</li>
+ * <li>all: at startup plugins are initialized and at exit they're shutdown; don't have test result for this</li>
+ * <li>all: in the Console, type 'TestPlugIn' and see it call something (or not... why doesn't this work?!)</li>
+ * </ul>
+ *
  */
 public class TestPlugIn implements PlugIn {
 
     private static boolean everInitialized = false;
 
-    private int initCount = 0;
+    private CoreSubsystem core;
     private TestUICallback testCallback;
 
-    public void init(CoreSubsystem core) throws Exception {
-        if (initCount > 0) {
-            throw new IllegalStateException("PlugIns have been initialized a second time without any shutdown call.");
-        }
-        initCount++;
 
-        // add a UICallback
+
+    public static void checkInitialized() {
+        if (!everInitialized) {
+            throw new IllegalStateException("The TestPlugIn was not ever initialized by core init.");
+        }
+    }
+
+
+
+
+    public void init(CoreSubsystem core) throws Exception {
+
+        everInitialized = true;
+        
+        this.core = core;
+
         if (testCallback == null ) {
             testCallback = new TestUICallback(core.getSettings().getMy().getNickname());
             core.addUICallback(testCallback);
         }
         // init, and check to make sure this isn't a repeated init
         if (testCallback.active) {
-            throw new IllegalStateException("We've got an init call but the UICallback is already active.");
+            throw new IllegalStateException("PlugIns have been initialized a second time without any shutdown call.");
         } else {
             testCallback.active = true;
         }
     }
 
-    public ConsolePlugInExtension getConsoleExtensions() {
-        return null;
-    }
-
     public void shutdown() throws Exception {
-        // don't worry if multiple shutdowns are called (at least for now)
-        if (initCount > 0) {
-            initCount--;
-        }
 
         if (testCallback.active) {
             testCallback.active = false;
@@ -61,11 +87,31 @@ public class TestPlugIn implements PlugIn {
         }
     }
 
-    public static void checkInitialized() {
-        if (!everInitialized) {
-            throw new IllegalStateException("The TestPlugIn was not ever initialized by core init.");
-        }
+
+
+
+
+
+
+
+
+    public ConsolePlugInExtension getConsoleExtensions() {
+        return new ConsolePlugInExtension() {
+            public boolean handleLine(String line, Printer print) {
+                if (line.equals("TestPlugIn")) {
+                    // run any callback method to force it through the chain
+                    core.getUICallback().isUIVisible();
+                    print.println("Finished calling UICallback method.");
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
     }
+
+
+
 
 
 
@@ -79,8 +125,9 @@ public class TestPlugIn implements PlugIn {
             this.nickname = nickname;
         }
         public void testDuplicateCallbacksInChain() {
-            //System.out.println("-------------------- TestUICallback for " + nickname + " was called!  Active: " + active + "  hash: " + this.hashCode() + " Method: " + Thread.currentThread().getStackTrace()[2]);
+            //System.out.println("-------------------- TestUICallback for " + nickname + " was called!  Active: " + active + "  hash: " + this.hashCode() + "  Method: " + Thread.currentThread().getStackTrace()[2]);
             if (active) {
+                System.out.println("Successful call by " + nickname + " (" + this.hashCode() + ") to method " + Thread.currentThread().getStackTrace()[2]);
                 Integer lastCallbackHash = hashCodeOfLastActiveCallbackInvokedForMain.get(nickname);
                 if (lastCallbackHash != null
                     && lastCallbackHash.intValue() != this.hashCode()) {
@@ -108,10 +155,6 @@ public class TestPlugIn implements PlugIn {
             testDuplicateCallbacksInChain();
         }
 
-        public void messageRecieved(int srcGuid, String message) {
-            testDuplicateCallbacksInChain();
-        }
-
         @Override
         public void searchHits(int srcGuid, int hops, List<SearchHit> hits) {
             testDuplicateCallbacksInChain();
@@ -129,6 +172,11 @@ public class TestPlugIn implements PlugIn {
 
         @Override
         public void statusMessage(String s) {
+            testDuplicateCallbacksInChain();
+        }
+
+        @Override
+        public void statusMessage(String s, boolean b) {
             testDuplicateCallbacksInChain();
         }
 
@@ -178,18 +226,6 @@ public class TestPlugIn implements PlugIn {
             testDuplicateCallbacksInChain();
         }
 
-        public void signalFileDatabaseFlushStarting() {
-            testDuplicateCallbacksInChain();
-        }
-
-        public void signalFileDatabaseFlushComplete() {
-            testDuplicateCallbacksInChain();
-        }
-
-        @Override
-        public void statusMessage(String s, boolean b) {
-            testDuplicateCallbacksInChain();
-        }
     }
 
 }
